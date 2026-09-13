b4fbbf812cbae2d95de903ca9a24826763e066d3
JustTrying652 <ngataevans@gmail.com>
2026-09-13 20:19:05 +0300

    typo fix

--- backend/server.js
+++ backend/server.js
@@ -19,7 +19,7 @@ app.get('/api/health', (req, res) => {
 })
 
 app.use('/api/auth', authRoutes)
-app.use('/api/questionsets', questionSetRoutes)
+app.use('/api/question-sets', questionSetRoutes)
 
 
 const server = http.createServer(app)
